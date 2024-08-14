a19c6921229724eb8fe66c5507b282c92a9b867d
pcsishun <pcsishun@gmail.com>
2024-08-14 10:37:41 +0700

    update

--- wellbreath.py
+++ wellbreath.py
@@ -1,9 +1,11 @@
-class WellBreath:
+import time
 
+class WellBreath:
     def __init__(self, 
                  temp, 
                  humid, 
                  co2,
+                 debug = False, ## default logging is False // not show log
                  thres_co2_mor_than = 1000,
                  thres_co2_low_than = 750,
                  thres_temp_mor_than = 35,
@@ -20,6 +22,7 @@ class WellBreath:
         self.temp = temp
         self.humid = humid
         self.co2 = co2
+        self.debug = debug
         self.thres_co2_mor_than = thres_co2_mor_than
         self.thres_co2_low_than = thres_co2_low_than
         self.thres_temp_mor_than = thres_temp_mor_than
@@ -33,42 +36,61 @@ class WellBreath:
         self.set_channel_cv_supply_high = set_channel_cv_supply_high
 
     def func_wellbreath(self):
+        self.__func_logging(is_func="logic", action=["temp: "+ str(self.temp), "humid: "+ str(self.humid), "co2: "+ str(self.co2)])
         if self.co2 >= self.thres_co2_mor_than and self.temp < self.thres_temp_mor_than and self.humid < self.thres_humid_mor_than:
-            command = self.__func_create_command("on", [self.set_exhaust_name, self.thres_temp_lower_than])
-            return command
+            command = self.__func_create_command(action="on", array_command=[self.set_exhaust_name, self.thres_temp_lower_than])
+            self.__func_logging(is_func="cmd", action=command)
+            return command["srtv"], command["cv"], command["supply"]
         elif self.co2 >= self.thres_co2_mor_than and (self.temp >= self.thres_temp_mor_than or self.humid >= self.thres_humid_mor_than):
-            command = self.__func_create_command("on", [self.set_exhaust_name, self.set_supply_high_name])
-            return command
+            command = self.__func_create_command(action="on",array_command= [self.set_exhaust_name, self.set_supply_high_name])
+            self.__func_logging(is_func="cmd", action=command)
+            return command["srtv"], command["cv"], command["supply"]
         elif self.co2 < self.thres_co2_low_than and (self.temp >= self.thres_temp_mor_than or self.humid >= self.thres_humid_low_than):
-            command = self.__func_create_command("on", [self.set_exhaust_name, self.set_supply_high_name])
-            return command
+            command = self.__func_create_command(action="on",array_command= [self.set_exhaust_name, self.set_supply_high_name])
+            self.__func_logging(is_func="cmd", action=command)
+            return command["srtv"], command["cv"], command["supply"]
         elif self.co2 < self.thres_co2_low_than and self.temp < self.thres_temp_lower_than and self.humid < self.thres_humid_low_than:
-            command = self.__func_create_command("off", [])
-            return command
+            command = self.__func_create_command(action="off", array_command=[])
+            self.__func_logging(is_func="cmd", action=command)
+            return command["srtv"], command["cv"], command["supply"]
 
     def __func_create_command(self,action, array_command):
         if action == "on":
             if self.set_exhaust_name in array_command and self.set_supply_low_name in array_command:
                 return {
-                    "srtv":0, # default 
-                    "cv": 1, # exhaust fan
-                    "supply": self.set_channel_cv_supply_low, # supply fan
+                    "srtv":float(0), # default 
+                    "cv": float(1), # exhaust fan
+                    "supply": float(self.set_channel_cv_supply_low), # supply fan
             
                 }
             elif self.set_exhaust_name in array_command and self.set_supply_high_name in array_command:
                 return {
-                    "srtv":0, # default 
-                    "cv": 1, # exhaust fan
-                    "supply": self.set_channel_cv_supply_high, # supply fan
+                    "srtv":float(0), # default 
+                    "cv": float(1), # exhaust fan
+                    "supply": float(self.set_channel_cv_supply_high), # supply fan
                 }
         elif action == "off":
             return {
-                    "srtv": 0, # default 
-                    "cv": 0, # exhaust fan
-                    "supply": 0, # supply fan
+                    "srtv": float(0), # default 
+                    "cv": float(0), # exhaust fan
+                    "supply": float(0), # supply fan
                 }
         
-example = WellBreath(temp=35,humid=55,co2=400)
+    def __func_logging(self, is_func ,action):
+        if self.debug == True:
+            if is_func =="cmd":
+                current_time = time.time()
+                formatted_time = time.strftime("%d/%m/%Y %H:%M:%S", current_time)
+                print(formatted_time + "strv: " + str(action["strv"]) +"cv: " + str(action["cv"]),"supply: " +  str(action["supply"]))
+            elif is_func == "logic":
+                current_time = time.time()
+                formatted_time = time.strftime("%d/%m/%Y %H:%M:%S", current_time)
+                print(formatted_time)
+                print(action)
+            
+
+
+example = WellBreath(temp=55,humid=55,co2=400, debug=True)
 t, h, c = example.func_wellbreath()
 
 print(t)
